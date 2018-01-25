7da979a6a81636bec0204df0ed02ebfe22239be4
Schulz, Dean <dpschu2@ilstu.edu>
2018-01-25 16:20:47 -0600

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,76 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { Observable } from 'rxjs/Rx';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  names = [];
+  current = "";
+  name = "";
+  button = "start";
+  added = false;
+  isenabled = true;
+  buttonColor = '#01DF01';
+  nextColor = '#345465';
+  index: any;
+  i = 0;
+  private subscription: Subscription;
+  constructor(public navCtrl: NavController, private tts: TextToSpeech) {
 
   }
-
+  next(){
+    if(this.button == "stop"){
+      this.index++;
+      if(this.index == this.names.length)
+        this.index = 0;
+      this.current = this.names[this.index];
+      this.i = 0;
+    }
+  }
+  setTimer(){
+    this.subscription = Observable.interval(2000).subscribe(x => {
+      if(this.i >= 3)
+          this.tts.speak(this.current);
+      this.i++;
+    });
+  }
+  cancelTimer(){
+    this.subscription.unsubscribe();
+  }
+  start(element){
+    if(this.names.length > 1){
+      
+      if(this.button == "start"){
+        this.setTimer();
+        this.isenabled = false;
+        this.index = 0;
+        this.button = "stop";
+        this.buttonColor = '#FF0000';
+        this.current = this.names[this.index];
+      }
+      else{
+        
+        this.names = [];
+        this.isenabled = true;
+        this.current = "";
+        this.added = false;
+        this.button = "start";
+        this.buttonColor = '#01DF01';
+        this.cancelTimer();
+      }
+    }
+  }
+  add(){
+    if(this.name.length != 0){
+      this.names.push(this.name);
+      this.name = "";
+      if(this.names.length > 0)
+        this.added = true;
+    }
+  }
 }
